d94ee0e10a45fc27dcc38b2e2a85221674d557c2
Guilherme FRANCISCO ALMEIDA <guilhermefranciscoalmeida22@gmail.com>
2025-04-17 15:19:48 -0300

    formulario conjunto crud

---
+++ FormularioSimplesConjunto/Form1.Designer.cs
@@ -0,0 +1,284 @@
+﻿
+namespace FormularioSimples
+{
+    partial class frmFormularioSimples
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNumeroCadastro = new System.Windows.Forms.TextBox();
+            this.txtNomeCompleto = new System.Windows.Forms.TextBox();
+            this.lblDataNasc = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.lblCidade = new System.Windows.Forms.Label();
+            this.comboBoxCidade = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rbNaoBinario = new System.Windows.Forms.RadioButton();
+            this.rbFeminino = new System.Windows.Forms.RadioButton();
+            this.rbMasculino = new System.Windows.Forms.RadioButton();
+            this.txtNomeSocial = new System.Windows.Forms.TextBox();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.lstCadastrados = new System.Windows.Forms.ListView();
+            this.txtPesquisa = new System.Windows.Forms.TextBox();
+            this.btnPesquisar = new System.Windows.Forms.Button();
+            this.btnCancelar = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtNumeroCadastro
+            // 
+            this.txtNumeroCadastro.Location = new System.Drawing.Point(134, 24);
+            this.txtNumeroCadastro.Name = "txtNumeroCadastro";
+            this.txtNumeroCadastro.Size = new System.Drawing.Size(242, 20);
+            this.txtNumeroCadastro.TabIndex = 0;
+            this.txtNumeroCadastro.Text = "Número Cadastro";
+            this.txtNumeroCadastro.Click += new System.EventHandler(this.LimpaNoClique);
+            // 
+            // txtNomeCompleto
+            // 
+            this.txtNomeCompleto.Location = new System.Drawing.Point(134, 50);
+            this.txtNomeCompleto.Name = "txtNomeCompleto";
+            this.txtNomeCompleto.Size = new System.Drawing.Size(242, 20);
+            this.txtNomeCompleto.TabIndex = 1;
+            this.txtNomeCompleto.Text = "Insira seu nome completo";
+            this.txtNomeCompleto.Click += new System.EventHandler(this.txtNomeCompleto_Click);
+            // 
+            // lblDataNasc
+            // 
+            this.lblDataNasc.AutoSize = true;
+            this.lblDataNasc.Location = new System.Drawing.Point(131, 113);
+            this.lblDataNasc.Name = "lblDataNasc";
+            this.lblDataNasc.Size = new System.Drawing.Size(105, 13);
+            this.lblDataNasc.TabIndex = 2;
+            this.lblDataNasc.Text = "Data de nascimento:";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dateTimePicker1.Location = new System.Drawing.Point(242, 113);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(78, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // lblCidade
+            // 
+            this.lblCidade.AutoSize = true;
+            this.lblCidade.Location = new System.Drawing.Point(134, 145);
+            this.lblCidade.Name = "lblCidade";
+            this.lblCidade.Size = new System.Drawing.Size(43, 13);
+            this.lblCidade.TabIndex = 4;
+            this.lblCidade.Text = "Cidade:";
+            // 
+            // comboBoxCidade
+            // 
+            this.comboBoxCidade.DropDownWidth = 223;
+            this.comboBoxCidade.FormattingEnabled = true;
+            this.comboBoxCidade.Items.AddRange(new object[] {
+            "Aracaju – Sergipe (SE)",
+            "Belém – Pará (PA)",
+            "Belo Horizonte – Minas Gerais (MG)",
+            "Boa Vista – Roraima (RR)",
+            "Brasília – Distrito Federal (DF) (capital federal)",
+            "Campo Grande – Mato Grosso do Sul (MS)",
+            "Cuiabá – Mato Grosso (MT)",
+            "Curitiba – Paraná (PR)",
+            "Florianópolis – Santa Catarina (SC)",
+            "Fortaleza – Ceará (CE)",
+            "Goiânia – Goiás (GO)",
+            "João Pessoa – Paraíba (PB)",
+            "Macapá – Amapá (AP)",
+            "Maceió – Alagoas (AL)",
+            "Manaus – Amazonas (AM)",
+            "Natal – Rio Grande do Norte (RN)",
+            "Palmas – Tocantins (TO)",
+            "Porto Alegre – Rio Grande do Sul (RS)",
+            "Porto Velho – Rondônia (RO)",
+            "Recife – Pernambuco (PE)",
+            "Rio Branco – Acre (AC)",
+            "Rio de Janeiro – Rio de Janeiro (RJ)",
+            "Salvador – Bahia (BA)",
+            "São Luís – Maranhão (MA)",
+            "São Paulo – São Paulo (SP)",
+            "Teresina – Piauí (PI)",
+            "Vitória – Espírito Santo (ES)"});
+            this.comboBoxCidade.Location = new System.Drawing.Point(184, 145);
+            this.comboBoxCidade.Name = "comboBoxCidade";
+            this.comboBoxCidade.Size = new System.Drawing.Size(158, 21);
+            this.comboBoxCidade.TabIndex = 5;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rbNaoBinario);
+            this.groupBox1.Controls.Add(this.rbFeminino);
+            this.groupBox1.Controls.Add(this.rbMasculino);
+            this.groupBox1.Location = new System.Drawing.Point(137, 183);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(250, 47);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Gênero";
+            // 
+            // rbNaoBinario
+            // 
+            this.rbNaoBinario.AutoSize = true;
+            this.rbNaoBinario.Location = new System.Drawing.Point(159, 20);
+            this.rbNaoBinario.Name = "rbNaoBinario";
+            this.rbNaoBinario.Size = new System.Drawing.Size(80, 17);
+            this.rbNaoBinario.TabIndex = 2;
+            this.rbNaoBinario.TabStop = true;
+            this.rbNaoBinario.Text = "Não-Binário";
+            this.rbNaoBinario.UseVisualStyleBackColor = true;
+            // 
+            // rbFeminino
+            // 
+            this.rbFeminino.AutoSize = true;
+            this.rbFeminino.Location = new System.Drawing.Point(7, 20);
+            this.rbFeminino.Name = "rbFeminino";
+            this.rbFeminino.Size = new System.Drawing.Size(67, 17);
+            this.rbFeminino.TabIndex = 1;
+            this.rbFeminino.TabStop = true;
+            this.rbFeminino.Text = "Feminino";
+            this.rbFeminino.UseVisualStyleBackColor = true;
+            // 
+            // rbMasculino
+            // 
+            this.rbMasculino.AutoSize = true;
+            this.rbMasculino.Location = new System.Drawing.Point(80, 20);
+            this.rbMasculino.Name = "rbMasculino";
+            this.rbMasculino.Size = new System.Drawing.Size(73, 17);
+            this.rbMasculino.TabIndex = 0;
+            this.rbMasculino.TabStop = true;
+            this.rbMasculino.Text = "Masculino";
+            this.rbMasculino.UseVisualStyleBackColor = true;
+            // 
+            // txtNomeSocial
+            // 
+            this.txtNomeSocial.Location = new System.Drawing.Point(134, 76);
+            this.txtNomeSocial.Name = "txtNomeSocial";
+            this.txtNomeSocial.Size = new System.Drawing.Size(242, 20);
+            this.txtNomeSocial.TabIndex = 7;
+            this.txtNomeSocial.Text = "Insira seu nome social";
+            this.txtNomeSocial.Click += new System.EventHandler(this.txtNomeSocial_Click);
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadastrar.Location = new System.Drawing.Point(479, 52);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(145, 44);
+            this.btnCadastrar.TabIndex = 8;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // lstCadastrados
+            // 
+            this.lstCadastrados.HideSelection = false;
+            this.lstCadastrados.Location = new System.Drawing.Point(102, 374);
+            this.lstCadastrados.MultiSelect = false;
+            this.lstCadastrados.Name = "lstCadastrados";
+            this.lstCadastrados.Size = new System.Drawing.Size(454, 169);
+            this.lstCadastrados.TabIndex = 9;
+            this.lstCadastrados.UseCompatibleStateImageBehavior = false;
+            this.lstCadastrados.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.lstCadastrados_ItemSelectionChanged);
+            // 
+            // txtPesquisa
+            // 
+            this.txtPesquisa.Location = new System.Drawing.Point(102, 304);
+            this.txtPesquisa.Name = "txtPesquisa";
+            this.txtPesquisa.Size = new System.Drawing.Size(338, 20);
+            this.txtPesquisa.TabIndex = 10;
+            // 
+            // btnPesquisar
+            // 
+            this.btnPesquisar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnPesquisar.Location = new System.Drawing.Point(479, 289);
+            this.btnPesquisar.Name = "btnPesquisar";
+            this.btnPesquisar.Size = new System.Drawing.Size(145, 45);
+            this.btnPesquisar.TabIndex = 11;
+            this.btnPesquisar.Text = "Pesquisar";
+            this.btnPesquisar.UseVisualStyleBackColor = true;
+            this.btnPesquisar.Click += new System.EventHandler(this.btnPesquisar_Click);
+            // 
+            // btnCancelar
+            // 
+            this.btnCancelar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold);
+            this.btnCancelar.Location = new System.Drawing.Point(479, 203);
+            this.btnCancelar.Name = "btnCancelar";
+            this.btnCancelar.Size = new System.Drawing.Size(145, 45);
+            this.btnCancelar.TabIndex = 12;
+            this.btnCancelar.Text = "Cancelar";
+            this.btnCancelar.UseVisualStyleBackColor = true;
+            this.btnCancelar.Click += new System.EventHandler(this.btnCancelar_Click);
+            // 
+            // frmFormularioSimples
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(724, 572);
+            this.Controls.Add(this.btnCancelar);
+            this.Controls.Add(this.btnPesquisar);
+            this.Controls.Add(this.txtPesquisa);
+            this.Controls.Add(this.lstCadastrados);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.txtNomeSocial);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.comboBoxCidade);
+            this.Controls.Add(this.lblCidade);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.lblDataNasc);
+            this.Controls.Add(this.txtNomeCompleto);
+            this.Controls.Add(this.txtNumeroCadastro);
+            this.Name = "frmFormularioSimples";
+            this.Text = "Formulário Simples";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumeroCadastro;
+        private System.Windows.Forms.TextBox txtNomeCompleto;
+        private System.Windows.Forms.Label lblDataNasc;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label lblCidade;
+        private System.Windows.Forms.ComboBox comboBoxCidade;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton rbNaoBinario;
+        private System.Windows.Forms.RadioButton rbFeminino;
+        private System.Windows.Forms.RadioButton rbMasculino;
+        private System.Windows.Forms.TextBox txtNomeSocial;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.ListView lstCadastrados;
+        private System.Windows.Forms.TextBox txtPesquisa;
+        private System.Windows.Forms.Button btnPesquisar;
+        private System.Windows.Forms.Button btnCancelar;
+    }
+}
+
---
+++ FormularioSimplesConjunto/Form1.cs
@@ -0,0 +1,345 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Globalization;
+using MySql.Data.MySqlClient;
+
+namespace FormularioSimples
+{
+    public partial class frmFormularioSimples : Form
+    {
+        // Conexão com o banco
+        MySqlConnection Conexao;
+        string data_source = "server=localhost;userid=root;password=;database=formulariosimples;";
+
+        public frmFormularioSimples()
+        {
+            InitializeComponent();
+
+            //Configuração inicial do ListView para exibição dos dados dos clientes
+            lstCadastrados.View = View.Details;         //Define a visualização como "Detalhes"
+            lstCadastrados.LabelEdit = true;           //Permite editar os títulos das colunas
+            lstCadastrados.AllowColumnReorder = true; //Permite reordenar as colunas
+            lstCadastrados.FullRowSelect = true;     //Seleciona a linha inteira ao clicar
+            lstCadastrados.GridLines = true;        //Exibe as Linhas de grade no listview
+
+            //Definindo as colunas do ListView
+            lstCadastrados.Columns.Add("numeroCadastro", 100, HorizontalAlignment.Left); //Coluna de código
+            lstCadastrados.Columns.Add("Nome Completo", 200, HorizontalAlignment.Left); //Coluna de Nome Completo
+            lstCadastrados.Columns.Add("Nome Social", 200, HorizontalAlignment.Left); //Coluna de nome social
+            lstCadastrados.Columns.Add("Data de Nascimento", 200, HorizontalAlignment.Left); //Coluna de e-mail
+            lstCadastrados.Columns.Add("Cidade", 200, HorizontalAlignment.Left); //Coluna de CPF
+            lstCadastrados.Columns.Add("Gênero", 200, HorizontalAlignment.Left);
+           
+        }
+
+        private void PesquisarPorNome(string nome)
+        {
+          string query = @"SELECT * FROM dados 
+                WHERE nomecompleto LIKE @termo 
+                   OR nomesocial LIKE @termo
+                   OR cidade LIKE @termo
+                ORDER BY numerocadastro DESC";
+
+            try
+            {
+                using (MySqlConnection conexao = new MySqlConnection(data_source))
+                {
+                    conexao.Open();
+
+                    MySqlCommand cmd = new MySqlCommand(query, conexao);
+                    cmd.Parameters.AddWithValue("@termo", "%" + nome + "%");
+
+                    MySqlDataReader reader = cmd.ExecuteReader();
+
+                    lstCadastrados.Items.Clear();
+
+                    while (reader.Read())
+                    {
+                        string[] row =
+                        {
+                    reader["numerocadastro"].ToString(),
+                    reader["nomecompleto"].ToString(),
+                    reader["nomesocial"].ToString(),
+                    Convert.ToDateTime(reader["datanascimento"]).ToString("dd/MM/yyyy"),
+                    reader["cidade"].ToString(),
+                    reader["genero"].ToString()
+                };
+
+                        lstCadastrados.Items.Add(new ListViewItem(row));
+                    }
+                }
+            }
+         
+        
+
+            catch (MySqlException ex)
+            {
+                //Trata erros relacionados ao MySQL
+                MessageBox.Show("Erro " + ex.Number + " ocorreu: " + ex.Message,
+                    "Erro",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+            catch (Exception ex)
+            {
+                //Trata outros tipos de erro * erros no backEnd do formulário
+                MessageBox.Show("Ocorreu: " + ex.Message,
+                    "Erro",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+
+            }
+            finally
+            {
+                //Garante que a conexão com o banco será fechada, mesmo se ocorrer erro
+                if (Conexao != null && Conexao.State == ConnectionState.Open)
+                {
+                    Conexao.Close();
+
+                }
+            }
+
+
+
+        }
+        private void carregar_clientes()
+        {
+                string query = "SELECT * FROM dados ORDER BY numerocadastro DESC";
+
+                try
+                {
+                    using (MySqlConnection conexao = new MySqlConnection(data_source))
+                    {
+                        conexao.Open();
+
+                        MySqlCommand cmd = new MySqlCommand(query, conexao);
+                        MySqlDataReader reader = cmd.ExecuteReader();
+
+                        lstCadastrados.Items.Clear();
+
+                        while (reader.Read())
+                        {
+                            string[] row =
+                            {
+                    reader["numerocadastro"].ToString(),
+                    reader["nomecompleto"].ToString(),
+                    reader["nomesocial"].ToString(),
+                    Convert.ToDateTime(reader["datanascimento"]).ToString("dd/MM/yyyy"),
+                    reader["cidade"].ToString(),
+                    reader["genero"].ToString()
+                };
+
+                            lstCadastrados.Items.Add(new ListViewItem(row));
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Erro ao carregar clientes: {ex.Message}", "Erro",
+                                   MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            
+
+        }
+
+
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            // Validações (mantenha as que você já tem)
+
+            if (btnCadastrar.Text == "Atualizar" && lstCadastrados.SelectedItems.Count > 0)
+            {
+                AtualizarCliente();
+            }
+            else
+            {
+                CadastrarNovoCliente();
+            }
+        }
+
+        private void CadastrarNovoCliente()
+        {
+            // Mantenha sua implementação atual de cadastro
+            // ...
+
+            // Após cadastrar com sucesso:
+            btnCadastrar.Text = "Cadastrar"; // Garante que o botão volte ao estado normal
+        }
+
+        private void AtualizarCliente()
+        {
+            if (lstCadastrados.SelectedItems.Count == 0) return;
+
+            int codigo = Convert.ToInt32(lstCadastrados.SelectedItems[0].SubItems[0].Text);
+
+            try
+            {
+                using (MySqlConnection conexao = new MySqlConnection(data_source))
+                {
+                    conexao.Open();
+
+                    string query = @"UPDATE dados SET 
+                           nomecompleto = @nomecompleto,
+                           nomesocial = @nomesocial,
+                           datanascimento = @datanascimento,
+                           cidade = @cidade,
+                           genero = @genero
+                           WHERE numerocadastro = @codigo";
+
+                    MySqlCommand cmd = new MySqlCommand(query, conexao);
+
+                    // Adiciona parâmetros
+                    cmd.Parameters.AddWithValue("@codigo", codigo);
+                    cmd.Parameters.AddWithValue("@nomecompleto", txtNomeCompleto.Text.Trim());
+                    cmd.Parameters.AddWithValue("@nomesocial", txtNomeSocial.Text.Trim());
+                    cmd.Parameters.AddWithValue("@datanascimento", dateTimePicker1.Value.ToString("yyyy-MM-dd"));
+                    cmd.Parameters.AddWithValue("@cidade", comboBoxCidade.Text.Trim());
+
+                    string generoSelecionado = "Não selecionado";
+                    if (rbFeminino.Checked) generoSelecionado = "Feminino";
+                    else if (rbMasculino.Checked) generoSelecionado = "Masculino";
+                    else if (rbNaoBinario.Checked) generoSelecionado = "Não Binário";
+
+                    cmd.Parameters.AddWithValue("@genero", generoSelecionado);
+
+                    int linhasAfetadas = cmd.ExecuteNonQuery();
+
+                    if (linhasAfetadas > 0)
+                    {
+                        MessageBox.Show("Cliente atualizado com sucesso!", "Sucesso",
+                                      MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                        // Atualiza a lista
+                        carregar_clientes();
+
+                        // Limpa os campos e volta o botão ao estado normal
+                        LimparCampos();
+                        btnCadastrar.Text = "Cadastrar";
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Erro ao atualizar: {ex.Message}", "Erro",
+                               MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void LimparCampos()
+        {
+            txtNumeroCadastro.Text = "";
+            txtNomeCompleto.Text = "";
+            txtNomeSocial.Text = "";
+            dateTimePicker1.Value = DateTime.Now;
+            comboBoxCidade.SelectedIndex = -1;
+            rbFeminino.Checked = false;
+            rbMasculino.Checked = false;
+            rbNaoBinario.Checked = false;
+        }
+        private void txtNomeCompleto_Click(object sender, EventArgs e)
+        {
+            if (txtNomeCompleto.Text == "Insira seu nome completo")
+            {
+                //"LIMPA TUTOOOOOO" -Pastor Metralhadora
+                txtNomeCompleto.Text = "";
+            }
+        }
+
+        private void LimpaNoClique(object sender, EventArgs e)
+        {
+            if (txtNumeroCadastro.Text == "Número Cadastro")
+            {
+                //"LIMPA TUTOOOOOO" -Pastor Metralhadora
+                txtNumeroCadastro.Text = "";
+            }
+        }
+
+        private void txtNomeSocial_Click(object sender, EventArgs e)
+        {
+            if (txtNomeSocial.Text == "Insira seu nome social")
+            {
+                //"LIMPA TUTOOOOOO" -Pastor Metralhadora
+                txtNomeSocial.Text = "";
+            }
+        }
+
+        private void MensagemDeErro(Control txt)
+        {
+            txt.BackColor = System.Drawing.Color.LightPink;
+            MessageBox.Show("Por favor, preencha os dados em rosa", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            return;
+        }
+
+        private void btnPesquisar_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(txtPesquisa.Text))
+            {
+                PesquisarPorNome(txtPesquisa.Text.Trim());
+            }
+            else
+            {
+                // Se estiver vazio, recarrega todos os registros
+                carregar_clientes();
+            }
+        }
+
+        private void btnCancelar_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+            btnCadastrar.Text = "Cadastrar";
+            lstCadastrados.SelectedItems.Clear();
+        }
+
+        private void lstCadastrados_ItemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e)
+        {
+           
+                // Verifica se há algum item selecionado
+                if (lstCadastrados.SelectedItems.Count == 0) return;
+
+                try
+                {
+                    // Obtém o item selecionado
+                    ListViewItem selectedItem = lstCadastrados.SelectedItems[0];
+
+                    // Preenche os campos do formulário
+                    txtNumeroCadastro.Text = selectedItem.SubItems[0].Text;
+                    txtNomeCompleto.Text = selectedItem.SubItems[1].Text;
+                    txtNomeSocial.Text = selectedItem.SubItems[2].Text;
+
+                    // Converte a data do formato exibido (dd/MM/yyyy)
+                    if (DateTime.TryParseExact(selectedItem.SubItems[3].Text, "dd/MM/yyyy",
+                        CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime dataNascimento))
+                    {
+                        dateTimePicker1.Value = dataNascimento;
+                    }
+
+                    // Preenche a cidade
+                    comboBoxCidade.Text = selectedItem.SubItems[4].Text;
+
+                    // Configura o gênero
+                    string genero = selectedItem.SubItems[5].Text;
+                    rbFeminino.Checked = genero.Contains("Feminino");
+                    rbMasculino.Checked = genero.Contains("Masculino");
+                    rbNaoBinario.Checked = genero.Contains("Não Binário");
+
+                    // Muda o botão para modo de atualização
+                    btnCadastrar.Text = "Atualizar";
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Erro ao carregar dados: {ex.Message}", "Erro",
+                                   MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            
+        }
+    }
+
+}
